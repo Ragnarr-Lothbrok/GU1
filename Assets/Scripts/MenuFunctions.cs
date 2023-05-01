c63554a14da71eba72c2934e7611e35ffb461832
Ragnarr-Lothbrok <brennja06@outlook.com>
2023-05-01 11:02:35 +0100

    MenuFunctions
    start of menu functions

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -8,7 +8,7 @@ public class MenuFunctions : MonoBehaviour
 {
       public void StartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("");
     }
 
     public void QuitGame()
