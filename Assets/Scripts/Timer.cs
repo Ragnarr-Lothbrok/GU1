05738d842ba1ac2f5448ca3ffb07f28c77760a63
Fraxxy <fra-xur@outlook.com>
2023-05-04 11:09:25 +0100

    Working timer and Splash Screen
    Timer fully working and cutting to the splash screen

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,15 +5,19 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    //  public Text timerText;
-    public TextMeshProUGUI tmpGUI;
-    private float startTime;
-    private bool timerIsRunning = false;
-    public float timeLimit = 900f; // 15 minutes in seconds
+    public TextMeshProUGUI tmpGUI; // Reference to the TextMeshProUGUI component that displays the time
+    private float startTime; // The time when the timer starts
+    private bool timerIsRunning = false; // Flag to indicate if the timer is currently running
+    private float originalTimeLimit = 900f; // The original time limit for the timer in seconds
+    private float timeLimit; // The current time limit for the timer in seconds
 
     void Start()
     {
+        // Initialize the time limit and start time
+        timeLimit = originalTimeLimit;
         startTime = Time.time;
+
+        // Set the timer flag to true
         timerIsRunning = true;
     }
 
@@ -21,19 +25,39 @@ public class Timer : MonoBehaviour
     {
         if (timerIsRunning)
         {
+            // Calculate the elapsed time since the timer started
             float t = Time.time - startTime;
 
-            string minutes = ((int)t / 60).ToString();
-            string seconds = (t % 60).ToString("f2");
+            // Convert the elapsed time to minutes and seconds format
+            string minutes = Mathf.FloorToInt(t / 60f).ToString("00");
+            string seconds = Mathf.FloorToInt(t % 60f).ToString("00");
 
+            // Update the TextMeshProUGUI component with the current time
             tmpGUI.text = "Time: " + minutes + ":" + seconds;
 
-          //  timerText.text = minutes + ":" + seconds;
-
+            // Check if the time limit has been reached
             if (t >= timeLimit)
             {
+                // If the time limit has been reached, load the Boss_Splash scene
                 SceneManager.LoadScene("Boss_Splash");
+
+                // Stop the timer by setting the flag to false
+                timerIsRunning = false;
             }
         }
+    }
+
+    public void Reset()
+    {
+        // Re-initialize the start time and timer flag
+        startTime = Time.time;
+        timerIsRunning = true;
+    }
+
+    public void SetTimeLimit(float newTimeLimit)
+    {
+        // Update the time limit and reset the timer
+        timeLimit = newTimeLimit;
+        Reset();
     }
 }
